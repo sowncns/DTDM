fc728743abbcb4aea657c8e9fd7863cd99f49456
sown <ngocson877469@gmail.com>
2025-11-26 00:16:42 +0700

    k

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT,"0.0.0.0", () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error("MongoDB connection error:", err));
