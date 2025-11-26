410fd724ac69f36801f9e5fa871dd677f0ba700d
sown <ngocson877469@gmail.com>
2025-11-26 17:05:57 +0700

    k

--- server.js
+++ server.js
@@ -7,15 +7,16 @@ const cors = require("cors");
 
 
 
-app.use(express.json());
-app.use(require("cors")());
 app.use(cors({
   origin: "*",
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: "Content-Type, Authorization"
 }));
 
-app.options("*", cors());
+
+
+app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
 
