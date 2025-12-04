dc2d0cc88634cfe93bbba73313befcf6fea0ed87
sown <ngocson877469@gmail.com>
2025-12-04 21:25:24 +0700

    fixxx

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ const cors = require("cors");
 
 
 
-app.use(cors({
-  origin: "*",
-  methods: "GET,POST,PUT,DELETE,OPTIONS",
-  allowedHeaders: "Content-Type, Authorization"
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);  // KHÔNG dùng 204
+  }
+
+  next();
+});
 
 
 app.use(express.json());
